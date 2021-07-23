69834e57890d040d541dc5a6a3e3e5916233d1e0
henriquealansantoss <henriquealansantoss@gmail.com>
2021-07-23 17:03:18 -0300

    Projetos7dias - B7Web_Prof: Bonieky Lacerda

---
+++ Desenhando no Canvas/script.js
@@ -0,0 +1,72 @@
+
+// initial data
+let currentColor = 'black';
+let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
+
+let screen = document.querySelector('#tela');
+let ctx = screen.getContext('2d'); //obtendo o contexto do elemento canvas
+
+
+// events
+document.querySelectorAll('.colorArea .color').forEach(item => {
+    item.addEventListener('click', colorClickEvent);
+})
+
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
+
+
+
+// functions
+function colorClickEvent(e){
+    let color = e.target.getAttribute('data-color');
+    currentColor = color;
+
+
+    document.querySelector('.color.active').classList.remove('active');
+    e.target.classList.add('active');
+}
+
+
+function mouseDownEvent(){
+  canDraw = true;
+  mouseX = e.pageX - screen.offsetLeft;
+  mouseY = e.pageY - screen.offsetTop;
+}
+
+function mouseMoveEvent(){
+    canDraw= false;
+}
+
+function mouseUpEvent(e){
+   if(canDraw){
+    //    let pointX = e.pageX - screen.offsetLeft;
+    //    let pointY = e.pageY - screen.offsetTop;
+    draw(e.pageX, e.pageY)
+
+   }
+}
+
+function draw(x,y){
+    let pointX = x - screen.offsetLeft;
+    let pointY = y - screen.offsetTop;
+
+
+    //configurações para desenhar
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = "round";
+    ctx.moveTo(mouseX,mouseY);
+    ctx.lineTo(pointX, pointY);
+    ctx.closePath();
+    ctx.strokeStyle = currentColor;
+    ctx.stroke();
+
+    
+    mouseX = pointX;
+    mouseY = pointY;
+
+}
