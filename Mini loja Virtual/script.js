67158dea92db1c26eaadb67ee497ab8718d13bfd
henriquealansantoss <henriquealansantoss@gmail.com>
2021-08-07 12:32:20 -0300

    Implementando a função de adicionar ao carrinho

--- Mini loja Virtual/script.js
+++ Mini loja Virtual/script.js
@@ -20,13 +20,46 @@ const itens = [{
 ];
 
 
-let inicializarLoja = ()=>{
+let inicializarLoja = () => {
     var containerProdutos = document.getElementById('produtos');
 
-    itens.map((val)=>{
-        console.log(val.nome);
+    itens.map((val) => {
+        containerProdutos.innerHTML += `
+        <div class = "produto-single">
+            <img src="` + val.img + `"/>
+            <p>` + val.nome + `</p>
+            <a key="` + val.id + `" href="#"> Adicionar ao carrinho! </a>
+        </div>
+        `
     })
 
 }
 
 inicializarLoja();
+
+let atualizarCarrinho = () => {
+    var containerCarrinho = document.getElementById('carrinho');
+    containerCarrinho.innerHTML = "";
+    itens.map((val) => {
+        if(val.quantidade > 0){
+            containerCarrinho.innerHTML += `
+            <p>`+val.nome+` | quantidade: `+val.quantidade+`</p>
+            <hr>
+        `
+        }
+        
+});
+
+}
+
+
+let links = document.querySelectorAll('a');
+
+for(let i = 0; i < links.length; i++){
+   links[i].addEventListener('click', function(){
+        let key = this.getAttribute('key');
+        itens[key].quantidade++;
+        atualizarCarrinho();
+        return false;
+   })
+};
