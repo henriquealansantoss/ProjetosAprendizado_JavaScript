53d46a8e33bb02ac7a4816029196dd495a861dd8
henriquealansantoss <henriquealansantoss@gmail.com>
2021-08-12 16:18:10 -0300

    Cronometro JS Puro

---
+++ Cronometro/script.js
@@ -0,0 +1,44 @@
+let display = document.getElementById('display');
+let minutos = document.getElementById('minutos');
+let segundos = document.getElementById('segundos');
+let comecar = document.getElementById('comecar');
+
+let cronometroSeg;
+let minutoAtual;
+let segundoAtual;
+let interval;
+
+
+for (let i = 0; i <= 60; i++) {
+    minutos.innerHTML += `<option value='${i}'> ${i}</option>`;
+}
+
+for (let i = 1; i <= 60; i++) {
+    segundos.innerHTML += `<option value='${i}'> ${i}</option>`;
+}
+
+comecar.addEventListener('click', function(){
+    minutoAtual = minutos.value;
+    segundoAtual = segundos.value;
+
+    display.childNodes[1].innerHTML = `${minutoAtual}:${segundoAtual}`;
+
+    interval = setInterval(() => {
+        segundoAtual--;
+        if (segundoAtual <= 0) {
+            if (minutoAtual > 0) {
+                minutoAtual--;
+                segundoAtual = 59;
+            }else{
+                alert('acabou o tempo!');
+                document.getElementById('sound').play();
+                clearInterval(interval); //acabando o cronometro, limpa e para de ser executado o interval
+            }
+        }
+
+        display.childNodes[1].innerHTML = `${minutoAtual}:${segundoAtual}`;
+
+    }, 1000);
+
+
+})
