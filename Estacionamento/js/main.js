4d659babce08dc6bdbc8a3d7feaccec934d7fbde
henriquealansantoss <henriquealansantoss@gmail.com>
2021-08-22 10:19:47 -0300

    Implementando funções de mostrar carros no patio

--- Estacionamento/js/main.js
+++ Estacionamento/js/main.js
@@ -14,4 +14,42 @@ function cadastraVeiculo(event) {
         minutos: time.getMinutes(),
         segundos: time.getSeconds()
     }
+
+
+    if (localStorage.getItem('patio') === null) {
+        let carros = [];
+        carros.push(carro);
+        localStorage.setItem('patio', JSON.stringify(carros));
+    } else {
+        let carros = JSON.parse(localStorage.getItem('patio'));
+        carros.push(carro);
+        localStorage.setItem('patio', JSON.stringify(carros));
+    }
+
+    mostrarPatio();
+
+}
+
+function mostrarPatio() {
+    let carros = JSON.parse(localStorage.getItem('patio'));
+    let carrosResultado = document.querySelector("#resultados");
+
+    carrosResultado.innerHTML = "";
+
+
+    for (let i = 0; i < carros.length; i++) {
+        let modelo = carros[i].modelo;
+        let placa = carros[i].placa;
+        let hora = carros[i].hora;
+        let minutos = carros[i].minutos;
+        let segundos = carros[i].segundos;
+
+
+        carrosResultado.innerHTML += `<tr>
+                                <td> ${modelo} </td>
+                                <td> ${placa} </td>
+                                <td> ${hora}:${minutos}:${segundos}</td>
+                                </tr>
+                                `;
+    }
 }
