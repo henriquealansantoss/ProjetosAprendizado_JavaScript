d73a85b4f1d3aa735e7a45cd163d86bccdcbaad6
henriquealansantoss <henriquealansantoss@gmail.com>
2021-08-30 23:04:38 -0300

    Implementando o armazenamento dos lembretes 'localStorage'

--- Agenda de tarefas/js/script.js
+++ Agenda de tarefas/js/script.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function(){
-    console.log(this);
+document.addEventListener('DOMContentLoaded', function () {
+    // console.log(this);
 
     // mostrarError();
     document.getElementById('buttonSave').onclick = criarLembrete;
@@ -7,17 +7,17 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
 //função para verificar se é há texto...
-function textoValido(texto){
-        if(texto === null || texto === "" || texto.lenght < 1){
-            return false;
-        }else{
-            return true;
-        }
+function textoValido(texto) {
+    if (texto === null || texto === "" || texto.lenght < 1) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 
 //função para exibir os erros
-function mostrarError(){
+function mostrarError() {
     let html = '';
     html += '<div class="alert alert-danger" role="alert">';
     html += 'Por favor informe a tarefa!';
@@ -28,17 +28,75 @@ function mostrarError(){
 }
 
 //função para limpar error
-function limparErro(){
+function limparErro() {
     document.getElementById('error').innerHTML = "";
 }
 
 //função para para criar o lembrete
-function criarLembrete(){
+function criarLembrete() {
     let conteudoTextArea = document.getElementById('texto').value;
-    if(!textoValido(conteudoTextArea)){
+    if (!textoValido(conteudoTextArea)) {
         mostrarError();
         return;
     }
 
     limparErro();
+
+    //criando variaveis para tempo...
+    let referencia = new Date();
+    let id = referencia.getTime();
+    let data = referencia.toLocaleDateString();
+    let texto = conteudoTextArea;
+
+
+    let recordatorio = {
+        "id": id,
+        "data": data,
+        "texto": texto
+    };
+
+
+    //função para verificar se existe o lembrete
+    comprovarRecordatorio(recordatorio);
+
+
+}
+
+//função para verificar se existe o lembrete
+function comprovarRecordatorio(recordatorio){
+    let recordatorioExistetes = localStorage.getItem("recordatorio");
+    if(recordatorioExistetes === null || recordatorioExistetes === ""){
+        let recordatorios = [];
+        recordatorios.push(recordatorio);
+
+        //salvar recordatorio
+        saveRecordatorios(recordatorios);
+    }else{
+        let recordatoriosRecuperados = JSON.parse(recordatorioExistetes);
+
+        //salvar
+        recordatoriosRecuperados.push(recordatorio);
+        saveRecordatorios(recordatorios);
+    }
+
+}
+
+
+//função para salva
+function saveRecordatorios(recordatorios){
+    let recordatoriosJSON = JSON.stringify(recordatorios);
+    localStorage.setItem("re",recordatoriosJSON);
+}
+
+//função para exibir itens
+function mostrarRecordatorios(){
+    let html = "";
+    let recordatorioExistetes = localStorage.getItem("recordatorios");
+    if(recordatorioExistetes === null || recordatorioExistetes === ""){
+        html = "Não existe nenhum lembrete...";
+
+        document.getElementById('recordatorios').innerHTML = ;
+    }
+
+
 }
